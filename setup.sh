369a348eff62d583bbe19d622906fdb9e5d913fe
gonzales <sergio.gonzalez.fernandez@cern.ch>
2020-05-05 21:57:16 +0200

    Added keras to setup

--- setup.sh
+++ setup.sh
@@ -3,4 +3,9 @@ lsetup "lcgenv -p LCG_94python3 x86_64-centos7-gcc7-opt ROOT"
 lsetup "lcgenv -p LCG_94python3 x86_64-centos7-gcc7-opt root_numpy"
 lsetup "lcgenv -p LCG_94python3 x86_64-centos7-gcc7-opt pandas"
 lsetup "lcgenv -p LCG_94python3 x86_64-centos7-gcc7-opt matplotlib"
+lsetup "lcgenv -p LCG_94python3 x86_64-centos7-gcc7-opt keras"
+lsetup "lcgenv -p LCG_94python3 x86_64-centos7-gcc7-opt tensorflow"
+lsetup "lcgenv -p LCG_94python3 x86_64-centos7-gcc7-opt keras_applications"
+lsetup "lcgenv -p LCG_94python3 x86_64-centos7-gcc7-opt keras_preprocessing"
+
 
